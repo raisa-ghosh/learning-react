df9eb8bb6cbb5771bd18c221c796c281dda39a25
raisa-ghosh <raisa.ghosh@juspay.in>
2024-12-13 17:26:06 +0530

    uyh

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Lets Start!</h1>
-    <p>Raisa learning React.</p>
+    <p>Raisa lgbghearning React.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
